f53eb47f3a3ffaadd854588d0f2906f7a12083ab
Thorsten Kelterborn <tk@friendsandeye.de>
2026-07-20 12:09:18 +0200

    Put some more facts into it and fix UTF-8 problem with Darwin's fox

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -23,13 +23,26 @@ def serialize_animal(animal):
     
     single_animal_output += '    <p class="card__text">\n'
     
+    scientific_name_value = animal.get("taxonomy", {}).get("scientific_name", "")
+    if scientific_name_value:
+        single_animal_output += f"        <strong>Scientific name:</strong> {scientific_name_value}<br/>\n"
+        
+    color_value = animal.get("characteristics", {}).get("color", "")
+    if color_value:
+        single_animal_output += f"        <strong>Color:</strong> {color_value}<br/>\n"
+        
     diet_value = animal.get("characteristics", {}).get("diet", "")
     if diet_value:
         single_animal_output += f"        <strong>Diet:</strong> {diet_value}<br/>\n"
     
     locations_value = animal.get("locations", [])
     if locations_value:
-        single_animal_output += f"        <strong>Location:</strong> {locations_value[0]}<br/>\n"
+        if len(locations_value) == 1:
+            locations_str = locations_value[0]
+        else:
+            locations_str = ", ".join(locations_value[:-1]) + " and " + locations_value[-1]
+        
+        single_animal_output += f"        <strong>Location:</strong> {locations_str}<br/>\n"
     
     type_value = animal.get("characteristics", {}).get("type", "")
     if type_value:
