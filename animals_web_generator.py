53833dfedb560e2665feff3a5c4dd8692ff3211f
Thorsten Kelterborn <tk@friendsandeye.de>
2026-07-20 11:31:45 +0200

    Rename and cleaning the files to be nice

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,33 +1,69 @@
 import json
 
+
 def load_data(file_path):
     """ Loads a JSON file """
-    with open(file_path, "r") as handle:
+    with open(file_path, "r", encoding="utf-8") as handle:
         return json.load(handle)
-    
-def give_info():
-    animals_data = load_data("animals_data.json")
-    for animal in animals_data:
-        name_value = animal.get("name")
-        if name_value:
-            print(f"\nName: {name_value}")
-            
-        diet_value = animal.get("characteristics", {}).get("diet", "")
-        if diet_value:
-            print(f"Diet: {diet_value}")
-            
-        locations_value = animal.get("locations", [])
-        if locations_value:
-            print(f"Location: {locations_value[0]}")
-            
-        type_value = animal.get("characteristics", {}).get("type", "")
-        if type_value:
-            print(f"Type: {type_value}")
 
+def serialize_animal(animal):
+    """
+        Serializes an animal into a string by getting the wanted information. here it is
+        the name of the animal, the FIRST location and out of its characteristics the diet
+        and the type. Each one is checked, if there is the asked data or not. Along with the
+        needed html-tags it becomes a complete html string for one animal out of the JSON file,
+        which will be returned as single_animal_output.
+    """
+    
+    single_animal_output = ''
+    single_animal_output += '<li class="cards__item">\n'
+    name_value = animal.get("name")
+    if name_value:
+        single_animal_output += f'    <div class="card__title">{name_value}</div>\n'
+    
+    single_animal_output += '    <p class="card__text">\n'
+    
+    diet_value = animal.get("characteristics", {}).get("diet", "")
+    if diet_value:
+        single_animal_output += f"        <strong>Diet:</strong> {diet_value}<br/>\n"
+    
+    locations_value = animal.get("locations", [])
+    if locations_value:
+        single_animal_output += f"        <strong>Location:</strong> {locations_value[0]}<br/>\n"
+    
+    type_value = animal.get("characteristics", {}).get("type", "")
+    if type_value:
+        single_animal_output += f"        <strong>Type:</strong> {type_value}<br/>\n"
+    
+    single_animal_output += '    </p>\n'
+    
+    single_animal_output += '</li>\n'
+
+
+    return single_animal_output
+    
+    
+
+def make_string():
+    """
+        The function first put all the JSON data into "animals_data. Then it creates a string, called
+        "output". Then it iterates through all the animal data in the JSON file and sends each single
+        one to "serialize_animal". After the return, it appends all to "output" and creates a complete
+        string which can be copied directly into the html template for usage there. Finally, it returns
+        the complete "output" string to the main function to print it.
+    """
+    
+    animals_data = load_data("animals_data.json")
+    output = ''
+    for animal in animals_data:
+        output += serialize_animal(animal)
+        
+    return output
+        
+    
 def main():
-    # animals_data = load_data('animals_data.json')
-    # print(animals_data)
-    give_info()
+    print(make_string())
     
 if __name__ == "__main__":
     main()
+
